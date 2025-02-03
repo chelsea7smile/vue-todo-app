dd5f286251d0c8cdbb684aa0d214c1c641f8e5f3
Yevhenii Kucherenko <chelsea7smile@gmail.com>
2025-02-03 21:52:12 +0200

    add base url

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/vue-todo-app/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
